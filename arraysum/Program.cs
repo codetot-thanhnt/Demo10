6f3a57e5839d84541637750f8fdcaf4ef9d62ea2
Giang <giang.nh.1061@aptechlearning.edu.vn>
2022-09-22 21:10:02 +0700

    giang add arraysum

---
+++ arraysum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraysum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] example = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(Some(example));
+            Console.ReadLine();
+        }
+        static int Some(int[] arr)
+        {
+            var arrSum = 0;
+           //return arr.Sum();
+           for(int i = 0; i < arr.Length; i++)
+            {
+                arrSum += arr[i];
+            }
+           return arrSum;
+
+        }
+        
+    }
+}
